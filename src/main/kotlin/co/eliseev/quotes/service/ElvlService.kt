c32c60825fb9831508b28141a6374c88af4a405f
Alexey Eliseev <alexey_eliseev@epam.com>
2020-03-11 01:08:47 +0300

    Combine Interface and Implementation

--- src/main/kotlin/co/eliseev/quotes/service/ElvlService.kt
+++ src/main/kotlin/co/eliseev/quotes/service/ElvlService.kt
@@ -1,9 +1,22 @@
 package co.eliseev.quotes.service
 
 import co.eliseev.quotes.model.entity.Elvl
+import co.eliseev.quotes.repository.ElvlRepository
+import org.springframework.stereotype.Service
 
 interface ElvlService {
     fun getAll(): List<Elvl>
     fun getByIsin(isin: String): Elvl?
     fun merge(elvl: Elvl)
+}
+
+@Service
+class ElvlServiceImpl(private val elvlRepository: ElvlRepository) : ElvlService {
+
+    override fun getAll() = elvlRepository.findAll()
+
+    override fun getByIsin(isin: String) = elvlRepository.findByIsin(isin)
+
+    override fun merge(elvl: Elvl) = elvlRepository.merge(elvl)
+
 }
