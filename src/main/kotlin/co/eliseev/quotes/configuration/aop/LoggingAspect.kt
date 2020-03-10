2e453c5fcf55d8b1abdbe6d9ec0545075801011a
Alexey Eliseev <alexey_eliseev@epam.com>
2020-03-11 00:55:57 +0300

    remove redundant @EnableAspectJAutoProxy

--- src/main/kotlin/co/eliseev/quotes/configuration/aop/LoggingAspect.kt
+++ src/main/kotlin/co/eliseev/quotes/configuration/aop/LoggingAspect.kt
@@ -4,12 +4,10 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.slf4j.LoggerFactory
-import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.stereotype.Component
 
 @Aspect
 @Component
-@EnableAspectJAutoProxy
 class LoggingAspect {
 
     @Around(value = "within(co.eliseev.quotes..*)")
